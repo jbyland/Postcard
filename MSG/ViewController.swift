6076703095385299b135d6610c747ffe42920d26
Jacob John Byland <JakeByland@alie-renee-martinez-s-macbook.local>
2014-09-27 19:14:39 -0700

    Initial Commit

---
+++ MSG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSG
+//
+//  Created by Jacob John Byland on 9/27/14.
+//  Copyright (c) 2014 Jacob John Byland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
